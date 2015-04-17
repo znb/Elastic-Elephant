c193fe5bce4c74f12d38f1e969fd6f253d8167c5
Matt Erasmus <matt.erasmus@ticketmaster.co.uk>
2015-04-17 15:57:06 +0100

    Tidy up

--- GetInstances.py
+++ GetInstances.py
@@ -3,27 +3,26 @@
 
 from MaltegoTransform import *
 import sys
-import os
 import boto.ec2
 from init import load_credentials
 
 
 creds = load_credentials()
+
 REGION = sys.argv[1]
 
 m = MaltegoTransform()
 
 try:
     conn = boto.ec2.connect_to_region(REGION, aws_access_key_id=creds[0], aws_secret_access_key=creds[1])
-    reservations = conn.get_all_reservations()
+    instances = conn.get_all_instances()
 
-    for x in range(0, len(reservations)):
-        instances = reservations[x].instances
-        aid = instances[0].id
-        ent = m.addEntity("matterasmus.AmazonEC2Instance", aid)
-        ent.addAdditionalFields("Instance ID", "InstanceID", True, str(aid))
+    for x in range(0, len(instances)):
+        instance = instances[x].instances
+        ent = m.addEntity('matterasmus.AmazonEC2Instance', instance[0].id)
+        ent.addAdditionalFields("InstanceID", "Instance ID", "strict", str(instance[0].id))
     else:
-        m.addUIMessage("Completed")
+        m.addUIMessage("Completed.")
 except Exception as e:
     m.addUIMessage(str(e))
 
