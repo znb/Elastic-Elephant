54632d6554e279e953faaba0150d1065eaa23f30
Matt Erasmus <matt.erasmus@ticketmaster.co.uk>
2015-04-17 16:15:27 +0100

    Get the Instance state

---
+++ GetInstanceState.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# Instance Information
+
+from MaltegoTransform import *
+import sys
+import boto.ec2
+from init import load_credentials
+
+
+creds = load_credentials()
+
+instance_check = sys.argv[1]
+REGION = creds[2]
+
+m = MaltegoTransform()
+
+try:
+    conn = boto.ec2.connect_to_region(REGION, aws_access_key_id=creds[0], aws_secret_access_key=creds[1])
+    instances = conn.get_all_instances()
+
+    for x in range(0, len(instances)):
+        instance = instances[x].instances
+        if str(instance_check) in str(instance):
+            ent = m.addEntity('matterasmus.AmazonEC2InstanceState', str(instance[0].state))
+            ent.addAdditionalFields("variable", "State", True, str(instance[0].state))
+        else:
+            pass
+    else:
+        m.addUIMessage("Completed")
+except Exception as e:
+    m.addUIMessage(str(e))
+
+m.returnOutput()
