1ff97eae84e2cba7c02277e8b987961460c30bbf
Matt Erasmus <matt.erasmus@ticketmaster.co.uk>
2015-04-17 14:19:07 +0100

    Changes required...Maltego doesnt like multiple returns

--- GetSecurityGroupRules.py
+++ GetSecurityGroupRules.py
@@ -24,33 +24,14 @@ try:
             sg_name = conn.get_all_security_groups(group_ids=group_id)[0]
             # Match on our search string
             if str(sg_name).split(":")[1] == str(sec_group):
-                group_nums = len(i.instances[0].groups)
-                sg_name = conn.get_all_security_groups(group_ids=group_id)[0]
                 # Security Group Ingress Rules
-                sec_rules = conn.get_all_security_groups(group_ids=group_id)[0].rules
-                rule_nums = len(sec_rules)
-                ent = m.addEntity("matterasmus.AmazonEC2SecurityGroupRule", "Ingress Rules")
-                ent.addAdditionalFields("IngressRules", "Ingress Rules", "strict", "Ingress Rules")
-                for g in range(rule_nums):
-                    ent = m.getVar("IngressRules")
-                    ent = m.addEntity("matterasmus.AmazonEC2SecurityGroup", str(conn.get_all_security_groups(group_ids=group_id)[0].rules[g]))
-                    ent.addAdditionalFields("ProtocolPort", "Protocol Port", "strict", str(conn.get_all_security_groups(group_ids=group_id)[0].rules[g]))
-                    ent = m.getVar("ProtocolPort")
-                    ent = m.addEntity("maltego.IPv4Address", str(conn.get_all_security_groups(group_ids=group_id)[0].rules[g].grants))
-                    ent.addAdditionalFields("Source", "Source", "strict", str(conn.get_all_security_groups(group_ids=group_id)[0].rules[g].grants))
+                ingress= m.addEntity("matterasmus.AmazonEC2IngressRules", "Ingress Rules")
+                ingress.addAdditionalFields("IngressRules", "Ingress Rules", "strict", "Ingress Rules")
 
                 # Security Group Egress Rules
-                egress_rules = conn.get_all_security_groups(group_ids=group_id)[0].rules_egress
-                number_of_egress = len(egress_rules)
-                ent = m.addEntity("matterasmus.AmazonEC2SecurityGroupRule", "Egress Rules")
-                ent.addAdditionalFields("EgressRules", "Egress Rules", "strict", "Egress rules")
-                for k in range(number_of_egress):
-                    ent = m.getVar("EgressRules")
-                    ent = m.addEntity("matterasmus.AmazonEC2SecurityGroup", str(conn.get_all_security_groups(group_ids=group_id)[0].rules_egress[k]))
-                    ent.addAdditionalFields("ProtocolPort", "Protocol Port", "strict", str(conn.get_all_security_groups(group_ids=group_id)[0].rules_egress[k]))
-                    ent = m.getVar("ProtocolPort")
-                    ent = m.addEntity("maltego.IPv4Address", str(conn.get_all_security_groups(group_ids=group_id)[0].rules_egress[k].grants))
-                    ent.addAdditionalFields("Destination", "Destination", "strict", str(conn.get_all_security_groups(group_ids=group_id)[0].rules_egress[k].grants))
+                egress = m.addEntity("matterasmus.AmazonEC2EgressRules", "Egress Rules")
+                egress.addAdditionalFields("EgressRules", "Egress Rules", "strict", "Egress rules")
+
 
     m.addUIMessage("Completed.")
 
