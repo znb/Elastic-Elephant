52a555da73cc0e6d798dfc52f698fa3295e2fe04
Matt Erasmus <matt.erasmus@ticketmaster.co.uk>
2015-04-20 13:17:49 +0100

    Small fixes

--- Transforms/GetGlobalSecurityGroups.py
+++ Transforms/GetGlobalSecurityGroups.py
@@ -10,12 +10,10 @@ REGION = creds[2]
 
 m = MaltegoTransform()
 
-# TODO: Is there a better way to do this ?
 try:
     conn = boto.ec2.connect_to_region(REGION, aws_access_key_id=creds[0], aws_secret_access_key=creds[1])
 
     reservations = conn.get_all_instances()
-
     for i in reservations:
         group_nums = len(i.instances[0].groups)
         for z in range(group_nums):
@@ -23,13 +21,12 @@ try:
             sg_name = conn.get_all_security_groups(group_ids=group_id)[0]
             sec_rules = conn.get_all_security_groups(group_ids=group_id)[0].rules
             ent = m.addEntity('matterasmus.AmazonEC2SecurityGroupName', str(sg_name).split(":")[1])
-            ent.addAdditionalFields("SecurityGroup", "Group ID", "strict", str(group_id))
+            ent.addAdditionalFields("GroupID", "Group ID", "strict", str(group_id))
             rule_nums = len(sec_rules)
 
     m.addUIMessage("Completed.")
 
 except Exception as e:
     m.addUIMessage(str(e))
-
 
 m.returnOutput()
