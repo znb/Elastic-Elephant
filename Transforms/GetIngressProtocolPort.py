7c3d660efe7644387919b6cbe33623834362f698
Matt Erasmus <matt.erasmus@ticketmaster.co.uk>
2015-04-20 13:18:20 +0100

    Small fixes..thanks Adam

--- Transforms/GetIngressProtocolPort.py
+++ Transforms/GetIngressProtocolPort.py
@@ -9,11 +9,9 @@ from init import load_credentials
 creds = load_credentials()
 REGION = creds[2]
 
-
 m = MaltegoTransform()
-m.parseArguments(sys.argv[1])
+m.parseArguments(sys.argv)
 sec_group = m.getVar("GroupID")
-m.addUIMessage("Group ID: ", str(sec_group))
 
 try:
     conn = boto.ec2.connect_to_region(REGION, aws_access_key_id=creds[0], aws_secret_access_key=creds[1])
@@ -24,13 +22,13 @@ try:
         group_nums = len(i.instances[0].groups)
         for z in range(group_nums):
             group_id = i.instances[0].groups[z].id
-            sg_name = conn.get_all_security_groups(group_ids=group_id)[0]
-            if str(sg_name).split(":")[1] == str(sec_group):
+            if str(group_id) == str(sec_group):
                 sec_rules = conn.get_all_security_groups(group_ids=group_id)[0].rules
                 rule_nums = len(sec_rules)
                 for g in range(rule_nums):
                     ent = m.addEntity('matterasmus.AmazonEC2SecurityGroupPP', str(conn.get_all_security_groups(group_ids=group_id)[0].rules[g]))
-                    
+                    ent.addAdditionalFields("GroupID", "Group ID", "strict", str(group_id))
+
     m.addUIMessage("Completed.")
 
 except Exception as e:
