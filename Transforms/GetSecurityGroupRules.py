a8213f49f957af74c8da83161a83d142fb5319c7
Matt Erasmus <matt.erasmus@ticketmaster.co.uk>
2015-04-20 13:19:16 +0100

    Small fixes

--- Transforms/GetSecurityGroupRules.py
+++ Transforms/GetSecurityGroupRules.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python
-# Pull all Security Group rule information for a Security Group
+# Create our Ingress and Egress rule chains
 
 from MaltegoTransform import *
 import boto.ec2
+import sys
 from init import load_credentials
 
 creds = load_credentials()
 REGION = creds[2]
 
-sec_group = sys.argv[1]
-
 m = MaltegoTransform()
+m.parseArguments(sys.argv)
+sec_group = m.getVar("GroupID")
 
 try:
     conn = boto.ec2.connect_to_region(REGION, aws_access_key_id=creds[0], aws_secret_access_key=creds[1])
 
     reservations = conn.get_all_instances()
-
     for i in reservations:
         group_nums = len(i.instances[0].groups)
         for z in range(group_nums):
             group_id = i.instances[0].groups[z].id
             sg_name = conn.get_all_security_groups(group_ids=group_id)[0]
-            if str(sg_name).split(":")[1] == str(sec_group):
+            if str(group_id) == str(sec_group):
                 ingress= m.addEntity("matterasmus.AmazonEC2IngressRules", "Ingress Rules")
                 ingress.addAdditionalFields("GroupID", "Group ID", "strict", str(group_id))
                 ingress.addAdditionalFields("SecurityGroup", "Group Name", "strict", str(sg_name).split(":")[1])
@@ -34,6 +34,5 @@ try:
 
 except Exception as e:
     m.addUIMessage(str(e))
-
 
 m.returnOutput()
