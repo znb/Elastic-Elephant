d3b4297a925ec8634fadfc03eec5208716c502ab
Matt <merasmus@opus2.com>
2017-06-05 14:14:47 +0100

    Fixed the name/display name booboo

--- Transforms/GetInstances.py
+++ Transforms/GetInstances.py
@@ -16,11 +16,11 @@ try:
     mt.addUIMessage("Getting instances in " + REGION)
     for instance in instances:
         ent = mt.addEntity('matterasmus.AmazonEC2Instance', instance.tags[0].get("Value"))
-        ent.addAdditionalFields("Instance Id", "InstanceId", "strict", str(instance.id))
-        ent.addAdditionalFields("Instance Type", "InstanceType", "strict", instance.instance_type)
-        ent.addAdditionalFields("Key Name", "KeyName", "strict", instance.key_name)
-        ent.addAdditionalFields("Private IP", "PrivateIp", "strict", instance.private_ip_address)
-        ent.addAdditionalFields("Region Name", "RegionName", "strict", REGION)
+        ent.addAdditionalFields("InstanceId", "Instance ID", "strict", str(instance.id))
+        ent.addAdditionalFields("InstanceType", "Instance Type", "strict", instance.instance_type)
+        ent.addAdditionalFields("KeyName", "Key Name", "strict", instance.key_name)
+        ent.addAdditionalFields("PrivateIp", "Private Ip", "strict", instance.private_ip_address)
+        ent.addAdditionalFields("RegionName", "Region Name", "strict", REGION)
     else:
         mt.addUIMessage("Completed.")
 except Exception as e:
