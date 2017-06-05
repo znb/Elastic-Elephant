a5bbe60e92515a4c0f68c8f7471b11a26f607a54
Matt <merasmus@opus2.com>
2017-06-05 12:51:49 +0100

    Blergh

--- Transforms/GetInstances.py
+++ Transforms/GetInstances.py
@@ -8,22 +8,22 @@ import boto3
 # Get our region name from Maltego
 REGION = sys.argv[1]
 
-m = MaltegoTransform()
+mt = MaltegoTransform()
 
 try:
     client = boto3.resource('ec2', region_name=REGION)
     instances = client.instances.all()
 
     for instance in instances:
-        ent = m.addEntity('matterasmus.AmazonEC2Instance', instance.tags[0].get("Value"))
+        ent = mt.addEntity('matterasmus.AmazonEC2Instance', instance.tags[0].get("Value"))
         ent.addAdditionalFields("Instance Id", "InstanceId", "strict", str(instance.id))
         ent.addAdditionalFields("Instance Type", "InstanceType", "strict", instance.instance_type)
         ent.addAdditionalFields("Key Name", "KeyName", "strict", instance.key_name)
         ent.addAdditionalFields("Private IP", "PrivateIp", "strict", instance.private_ip_address)
         ent.addAdditionalFields("Region Name", "RegionName", "strict", REGION)
     else:
-        m.addUIMessage("Completed.")
+        mt.addUIMessage("Completed.")
 except Exception as e:
     m.addUIMessage(str(e))
 
-m.returnOutput()
+mt.returnOutput()
